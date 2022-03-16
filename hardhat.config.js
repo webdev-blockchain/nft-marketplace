df4966a4b004b5aef7b4087349c98747b63fa803
Andy <webdev.blockchain@gmail.com>
2022-03-16 16:51:46 -0400

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -7,11 +7,11 @@ module.exports = {
     hardhat: {
       chainId: 1337
     },
-    mumbai: {
-      url: "https://rpc-mumbai.maticvigil.com",
-      accounts: [process.env.privateKey]
-      // accounts: [0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266]
-    }
+    // mumbai: {
+    //   url: "https://rpc-mumbai.maticvigil.com",
+    //   accounts: [process.env.privateKey]
+    //   // accounts: [0xf39fd6e51aad88f6f4ce6ab8827279cfffb92266]
+    // }
   },
   solidity: {
     version: "0.8.4",
